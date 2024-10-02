416bc92f8beb23dd77332e5206a2b82c9c37a3ae
Damodharkarthu123 <damodharkarthu123@gmail.com>
2024-10-02 18:51:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Phone from './iphone/Phone';
+
+function App() {
+  return (
+    <div className="App">
+      <Phone />
+    </div>
+  );
+}
+
+export default App;
