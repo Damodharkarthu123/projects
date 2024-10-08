63730f42a5d5504de93e9a9c0965c193444fe09b
Damodharkarthu123 <damodharkarthu123@gmail.com>
2024-10-08 12:31:21 +0530

    changes are  in files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 import Phone from './iphone/Phone';
 import Home from './home/Home';
 import { Route, Routes,route } from 'react-router-dom';
+import Firstpage from './home/Firstpage';
+import Secondpage from './home/Secondpage';
+import Thirdpage from './home/Thirdpage';
+import Fourthpage from './home/Fourthpage';
+import Fifthpage from './home/Fifthpage';
 
 function App() {
   return (
     <div className="App">
       {/*<Phone /> */}
       <Home />
+      <Routes>
+      <Route path='abcd' element = {<Firstpage />} />
+      <Route path='abcde' element = {<Secondpage />} />
+      <Route path='abcdef' element = {<Thirdpage />} />
+      <Route path=',abcdefg' element ={<Fourthpage />} />
+      <Route path=',abcdefgh' element ={<Fifthpage />} />
+
+      </Routes>
      
-     
+     <Firstpage />
+     <Secondpage />
+     <Thirdpage />
+     <Fourthpage />
+     <Fifthpage />
     </div>
   );
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Route, Routes,route } from 'react-router-dom';
+
 import { Link } from 'react-router-dom';
 import "./Home.css";
-import Firstpage from './Firstpage';
-import Secondpage from './Secondpage';
-import Thirdpage from './Thirdpage';
-import Fourthpage from './Fourthpage';
-import Fifthpage from './Fifthpage';
+
 
 const Home = () => {
   return (
@@ -25,19 +21,41 @@ const Home = () => {
         <img src='/Rectangle 119.png' alt='sd' className='su' />
         <img src='/Rectangle 1.png' alt='gsd' className='sa' />
         </div><hr />
-         <Routes>
-      <Route path='abcd' element = {<Firstpage />} />
-      <Route path='abcde' element = {<Secondpage />} />
-      <Route path='abcdef' element = {<Thirdpage />} />
-      <Route path=',abcdefg' element ={<Fourthpage />} />
-      <Route path=',abcdefgh' element ={<Fifthpage />} />
+        
+        <div className='court'>
+           <div className='super'>
+            <img src='/Bulb 2.png' alt='bulb' className='like' />
+          
+              <div className='re'><h1>Advanced software,advanced  <br /> reasearch for<span className='kulki'> SIMPLE </span>life</h1>  <br />
+              <h5>we harness the latest technologies to develop cutting-edge<br />digital solutions for modern business requirements</h5> <br /><br /><br />
+            
+              <img src='/Vector@3x.png' alt='vector' className='sek' /><br /> <br /> <br /><br />
+      <div className='jsx'><h2>1234</h2>
+      <h6>Lorem lpsum <br /> is simply. </h6>
+      </div>     
+      
+        <div className='jsx1'><h2>1234</h2>
+        <h6>Lorem lpsum <br /> is simply. </h6></div>
+        <img src='/bell_2 2.png' alt='rod' className='bc' />
 
-      </Routes>
-     <Firstpage />
-      <Secondpage />
-      <Thirdpage />
-      <Fourthpage />
-      <Fifthpage />
+
+         <div className='rew'>
+         <img src='/Group 60.png' alt='sd'  />
+
+          </div>  
+          <div className='lly'>
+            </div>  
+              
+              </div>
+                
+                </div> 
+
+        
+     </div>
+       
+
+             
+   
     </div>
   )
 }
