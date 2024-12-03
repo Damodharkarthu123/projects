0fd1c2aa9edb4af84dbe33c6e3b191245a6a7848
Damodharkarthu123 <damodharkarthu123@gmail.com>
2024-12-03 15:36:42 +0530

    added new files and changes

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,46 +1,53 @@
-
-
-import React from 'react'
-import styles from './Footer.module.css'
-import Footercard from './Footercard'
-
+import React from "react";
+import styles from "./Footer.module.css";
+import Footercard from "./Footercard";
 
 const Footer = () => {
-    const  Keys = [
-        {
-        name:'Lorem'
-        }
-    ]
-    const repeatKeys= Array(10).fill(Keys[0])
+  const Keys = [
+    {
+      name: "Lorem",
+    },
+  ];
+  const repeatKeys = Array(10).fill({ name: "Lorem" });
   return (
     <div className={styles.footer}>
-        <div className={styles.keywords}>
+      <div className={styles.keywords}>
         <div className={styles.heading}>Key words</div>
         <div className={styles.lorems}>
-            { repeatKeys.map((item,index)=>{
-                return <Footercard  key={`Footercard-${index}`}{...item}/>
-            })}
+          {repeatKeys.map((item, index) => {
+            return <Footercard key={`Footercard-${index}`} {...item} />;
+          })}
         </div>
+        <div className={styles.verticalline1}></div>
+      </div>
+      <div className={styles.ipsum}>
+        <div className={styles.title}>
+          <div>Lorem Ipsum is simply</div>
         </div>
-        <div className={styles.ipsum}>
-        <div className={styles.title}>Lorem Ipsum is simply</div>
-        <div className={styles.daigram}>
-            <img src='/ipsum.png' alt='ipsum' />
+        <button className={styles.daigram}>
+          <img src="/ipsum.png" alt="ipsum" />
+        </button>
+        <div className={styles.titles}>
+          <div>Lorem Ipsum is simply </div>
+          <div>Go to our official Announcements</div>
+        </div>
+      </div>
+      <div className={styles.verticalline2}></div>
+      <div className={styles.ipsumdummy}>
+        <div className={styles.requirement}>
+          <div>Lorem Ipsum is simply dummy</div>
+          <div>Go to DID requirements</div>
+        </div>
+        <div className={styles.simply}>
+          <div>Lorem Ipsum is simply</div>
+          <div>To the knowledgebase</div>
+        </div>
+      </div>
+      <div className={styles.social}>
+        <img src="/Sociallink.png" alt="sociallink" />
+      </div>
+    </div>
+  );
+};
 
-        </div>
-        <div className={styles.titles}>Lorem Ipsum is simply
-        <div className={styles.description}>Go to our official Announcements</div>
-        </div>
-        <div className={styles.ipsumdummy}>Lorem Ipsum is simply dummy</div>
-        <div className={styles.matter}>Go to DID requirements</div>
-        <div className={styles.simply}>Lorem Ipsum is simply</div>
-        <div className={styles.base}>To the knowledgebase</div>
-        <div className={styles.social}>
-            <img src='/Sociallink.png' alt='sociallink' />
-            </div>
-            </div>
-     </div>
-  )
-}
-
-export default Footer
+export default Footer;
